18d3146ceb61dcbec8d96a5975b320ea63ce2404
SanjayAlbiorix <sanjayp.albiorix@gmail.com>
2025-10-07 16:10:07 +0530

    status bar fixed

--- App.tsx
+++ App.tsx
@@ -1,11 +1,8 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import MainApp from "./app/MainApp";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./app/store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 const queryClient = new QueryClient();
 
 export default function App() {
--- app/MainApp.tsx
+++ app/MainApp.tsx
@@ -1,14 +1,26 @@
-import { StyleSheet, Text, useColorScheme, View } from "react-native";
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from "react-native";
 import { useEffect } from "react";
 import NavigationStack from "./NavigationStack";
 import { useDispatch, useSelector } from "react-redux";
-import { stateUserTheme, updateActiveTheme } from "./store/themeSlice";
+import {
+  stateIsLightTheme,
+  stateUserTheme,
+  updateActiveTheme,
+} from "./store/themeSlice";
+import colors from "../assets/colors";
 
 export default function MainApp() {
   const colorScheme = useColorScheme();
 
   const dispatch = useDispatch();
   const userChoice = useSelector(stateUserTheme);
+  const isLightTheme = useSelector(stateIsLightTheme);
 
   useEffect(() => {
     if (userChoice === "system") {
@@ -16,7 +28,15 @@ export default function MainApp() {
     }
   }, []);
 
-  return <NavigationStack />;
+  return (
+    <>
+      <StatusBar
+        barStyle={isLightTheme ? "dark-content" : "light-content"}
+        backgroundColor={isLightTheme ? colors.white : colors.black}
+      />
+      <NavigationStack />
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
